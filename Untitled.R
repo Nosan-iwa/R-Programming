ac614fcf04b7d5a5251e2326c7e506a10aa510b1
Nosan <limbunosan@gmail.com>
2026-07-22 13:09:49 +0530

    22-07-20226 Session-5

---
+++ Untitled.R
@@ -0,0 +1,10 @@
+arr <- array(1:24, dim = c(2, 3, 4))
+
+vec_as_array <- array(1:5, dim = 5)
+
+# Dimensions: 2(rows) x 3(columns) x 4(layers)
+# Total elements = 2 x 3 x 4 = 24
+
+arr
+vec_as_array
+vec_as_array
